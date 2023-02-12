07703edd07fd96bf166f00571c3af9118c50b36e
Mama africa <k_vashalomidze@mail.ru>
2023-02-12 11:10:23 +0400

    completed Customer class methods

--- src/Customer.java
+++ src/Customer.java
@@ -29,8 +29,13 @@ public class Customer {
     }
 
     // search book in customer's borrowed books
-    public Book searchBook(){
-        // TO DO
+    public Book searchBookByName(String name){
+        for(Book desiredBook : this.borrowedBooks){
+            if (name.equals(desiredBook.getName())){
+                return desiredBook;
+            }
+        }
+        return null;
     }
 
     // borrow book
@@ -41,15 +46,34 @@ public class Customer {
             // remove book from BookStorage after borrowing it
             BookStorage.getInstance().removeBook(localBookTemp);
             // add borrowed book to borrowed book's set
-            this.borrowedBooks.add(localBookTemp);
+            this.addBookInBorrowedBooks(localBookTemp);
         } else {
             System.out.println("There is no book with name '".concat(bookName).concat("' in library"));
         }
     }
 
+    // remove book from borrowed books
+    private void removeBookFromBorrowedBooks(Book book){
+        this.borrowedBooks.remove(book);
+    }
+
+    // add book in borrowed books
+    private void addBookInBorrowedBooks(Book book){
+        this.borrowedBooks.add(book);
+    }
+
     // return book
     public void returnBookWithName(String bookName){
-        // TO DO
+        // search book by name to return it in library storage
+        Book localBookTemp = this.searchBookByName(bookName);
+        if (localBookTemp != null){
+            // remove it from set of borrowed books
+            this.removeBookFromBorrowedBooks(localBookTemp);
+            //return it in library storage
+            BookStorage.getInstance().addNewBook(localBookTemp);
+        } else {
+            System.out.println("There is no such book borrowed to return ");
+        }
     }
 
 }
